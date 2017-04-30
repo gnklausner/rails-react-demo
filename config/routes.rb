f4570387eba29bc0e1c548dfb9f959fa313c25c4
G Klausner <gnklausner@gmail.com>
2017-04-30 17:02:57 -0400

    Tightened down route for contacts. Added root redirect to /contacts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :contacts
+  resources :contacts, only: [:index, :create, :destroy]
+  root :to => redirect('/contacts')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
